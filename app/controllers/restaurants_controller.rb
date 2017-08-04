9350cf5fb107749ceb497e6050e60777fff5ac3d
Jessica Titus <jtitus1195@gmail.com>
2017-08-04 13:45:52 -0400

    add restaurants show

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -6,4 +6,29 @@ class RestaurantsController < ApplicationController
   def new
     @restaurant = Restaurant.new
   end
+
+  def create
+  @restaurant = Restaurant.new(restaurant_params)
+
+    if @restaurant.save
+      flash[:notice] = 'Restaurant was successfully created'
+      redirect_to root_path
+    else
+      flash[:error] = 'Restaurant was not created successfully'
+      render :new
+    end
+  end
+end
+
+private
+
+def restaurant_params
+    params.require(:restaurant).permit(
+      :name,
+      :address,
+      :city,
+      :state,
+      :zip,
+      :img_url
+    )
 end
