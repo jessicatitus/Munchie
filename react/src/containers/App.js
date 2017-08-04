a1537191796cbc832070794d6af2daa4b8f0fa06
Jessica Titus <jtitus1195@gmail.com>
2017-08-04 10:53:25 -0400

    adding react

---
+++ react/src/containers/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Router, Route, browserHistory, IndexRoute } from 'react-router';
+
+const App = props => {
+  return (
+    <Router history={browserHistory}>
+    </Router>
+  )
+}
+
+export default App;
