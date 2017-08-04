9498f86a5e842df8c3f15d77a34a508652eaa890
Jessica Titus <jtitus1195@gmail.com>
2017-08-04 11:09:28 -0400

    add api endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
 
+  namespace :api do
+   namespace :v1 do
+     resources :restaurants, only: [:index]
+   end
+ end
+
   devise_for :users
   root 'static_pages#index'
 end
